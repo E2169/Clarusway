f1fd01d8219ec5cf7d006cd7566c6f5ac9845090
Leon <leonalispringer@gmail.com>
2020-07-07 18:42:41 +0300

    I wrote the code for If-Statements assignment_2

---
+++ Assignment-2_If-Statements_Covid-19.py
@@ -0,0 +1,18 @@
+print('Answer the following questions only yes or no;')
+age = input('Are you a cigarette addict older than 75 years old?: ').lower()
+age_bool = age == 'yes'
+
+chronic = input('Do you have a severe chronic disease?:').lower()
+chronic_bool = chronic == 'yes'
+
+immune = input('Is your immune system too weak?: ').lower()
+immune_bool = immune == 'yes'
+
+if (age != 'yes' and age != 'no') or (chronic != 'yes' and chronic != 'no') or (immune != 'yes' and immune != 'no'):
+    print('Something went wrong')
+    
+elif age_bool or chronic_bool or immune_bool:
+    print('You are in risky group')
+    
+else:
+    print('You are not in risky group')

8a0121aecf90d22e9895afdd4074b1fccb69555a
Leon <leonalispringer@gmail.com>
2020-07-12 03:56:41 +0300

    Here is the solution for Fibonacci numbers assignment.

---
+++ Assignment-5_Fibonacci_Numbers.py
@@ -0,0 +1,8 @@
+# Here is the solution of Fibonacci numbers assignment.
+
+fibonacci = [1, 1]
+
+while fibonacci[-1] < 55:
+    fibo_next = fibonacci[-2] + fibonacci[-1]
+    fibonacci.append(fibo_next)
+print(fibonacci)
